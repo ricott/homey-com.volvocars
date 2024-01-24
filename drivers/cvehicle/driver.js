7e9ebd55cef015c8735dc54bc1f7199978d26803
Richard Barkestam <richard@barkestam.se>
2024-01-24 16:30:21 +0100

    Repair supported

--- drivers/cvehicle/driver.js
+++ drivers/cvehicle/driver.js
@@ -70,6 +70,64 @@ class ConnectedVehicleDriver extends Homey.Driver {
             return devices;
         });
     }
+
+    onRepair(session, device) {
+        let self = this;
+
+        session.setHandler('settings', async (data) => {
+            if (data.username === '' || data.password === '' || data.vccApiKey === '') {
+                throw new Error('Username, password and vcc api key are mandatory!');
+            }
+
+            const token = await this.tokenManager.getToken(
+                Homey.env.VCC_LOGIN_TOKEN,
+                data.username,
+                data.password
+            )
+                .catch(reason => {
+                    return Promise.reject(reason);
+                });
+
+            const cVehicle = new ConnectedVehicle({
+                accessToken: token.access_token,
+                vccApiKey: data.vccApiKey,
+                device: this
+            });
+
+            return cVehicle.getVehicles()
+                .then(async function (vehicles) {
+                    const msg = `Vehicle '${device.getName()}' is not connected to the Volvo account '${data.username}'`;
+                    if (!Array.isArray(vehicles?.data)) {
+                        // The account doesnt have any linked vehicles
+                        self.error(msg);
+                        throw new Error(msg);
+                    }
+
+                    // Verify the new account has access to the vehicle being repaired
+                    const vehicle = vehicles.data.find(vehicle => vehicle.vin == device.getData().id);
+                    if (vehicle) {
+                        self.log(`Found vehicle with id '${vehicle.vin}'`);
+                        device.storeCredentialsEncrypted(data.username, data.password);
+                        await device.setSettings({
+                            vccApiKey: data.vccApiKey
+                        })
+                            .catch(err => {
+                                this.error(`Failed to update settings`, err);
+                            });
+
+                        session.done();
+                    } else {
+                        self.error(msg);
+                        throw new Error(msg);
+                    }
+                })
+                .catch(reason => {
+                    self.error(reason);
+                    return Promise.reject(reason);
+                });
+
+        });
+    }
 }
 
 module.exports = ConnectedVehicleDriver;
